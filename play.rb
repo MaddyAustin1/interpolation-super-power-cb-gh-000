086b6118adeb09a78f75ce86e0e476eb41221582
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-18 15:59:13 +0000

    Automatically backed up by Learn

--- play.rb
+++
@@ -1,8 +0,0 @@
-def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
-end
-
-maddy = ["Maddy", 24, "Data Engineer", "0783814193712"]
-
-print_business_card(maddy)
