d9f84659940766e294d4a703270baf92a809efb4
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-18 16:09:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colours)
-  puts colours[0].split.map(&:first) = colours[0]
+  puts colours[0].split.map(&:first) "=" colours[0]
 end
 
 rain = ["Red", "Yellow", "Pink"]
